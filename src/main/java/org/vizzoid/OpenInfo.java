141e8b0d286a7aca45e0a2a906ef162df0b77086
Vizzoid <82728229+Vizzoid@users.noreply.github.com>
2023-01-25 11:12:57 -0500

    Began OpenInfo

---
+++ src/main/java/org/vizzoid/OpenInfo.java
@@ -0,0 +1,24 @@
+public class OpenInfo {
+    
+    private final Info info;
+    
+    public static final OpenInfo EMPTY = new OpenInfo(null) {
+        public void start() {
+        }
+        public void end() {
+        }
+    }
+    
+    public OpenInfo(Info info) {
+        this.info = info;
+    }
+    
+    public void start() {
+        
+    }
+    
+    public void end() {
+        
+    }
+    
+}
